e2b0373fe863268df8e8c0bb98ac41fb0aac8eed
CptTiki <CptTiki@gmail.com>
2026-07-05 16:07:47 -0400

    Rooms and Level

---
+++ world/levels/base_level.gd
@@ -0,0 +1,11 @@
+extends Node3D
+class_name BaseLevel
+
+@onready var player_spawn: Node3D = $PlayerSpawn
+
+const PLAYER_PREFAB : PackedScene = preload("res://world/player/player.tscn")
+
+func _ready() -> void:
+	var player: Player = PLAYER_PREFAB.instantiate()
+	player.global_transform = player_spawn.global_transform
+	add_child(player)
